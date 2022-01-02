2b08c519b8553abe5fd3eaf6c30d884aff740b64
HybridTheory <bobr703@gmail.com>
2022-01-02 19:53:15 +0300

    Added some stupid comments

--- www/script.js
+++ www/script.js
@@ -8,6 +8,10 @@ let currentIndex = 0
 populateMemeArray()
 loadMore()
 
+
+// Load more function gets the array length and hides the button if there's nothing to load (shouldn't be a problem to a meme generator)
+// When a button is clicked, the for loop appends an image from an array which is populated beforehand
+
 function loadMore() {
     let maxResult = 1
 
@@ -22,6 +26,8 @@ function loadMore() {
     currentIndex += maxResult
 }
 
+// Function to populate the array with images located in a specific folder
+// The loop iterates through numbers and loads an image to an array slot with an according number
 function populateMemeArray() {
     for(let i = 0; i<5; i++) {
         memeArray[i] = new Image()
