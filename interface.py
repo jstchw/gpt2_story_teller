d4ce90f1ac7745fc0c5366b49809559d5824a992
jstchw <bobr703@gmail.com>
2022-01-28 19:11:10 +0000

    Model not uploaded, electron implemented

--- interface.py
+++ interface.py
@@ -4,5 +4,7 @@ import eel
 class Interface:
 
     def __init__(self):
+        eel.browsers.set_path('electron', '/Applications/Electron.app/Contents/MacOS/Electron')
+
         eel.init('www')
-        eel.start('index.html', size=(550, 570))
+        eel.start('index.html', mode='electron', size=(550, 570))
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from imgmanager import ImgManager
 from interface import Interface
+from nn import NN
 
 # Manages the memes
 im = ImgManager(input_img='meme0.jpg', output_img='test_img.gif', )
 im.reformat_image()
 
+network = NN('124M')
+
 # Creates an instance of a browser using Eel.
 gui = Interface()
 
+# text = im.read_text()
 
-#text = im.read_text()
-
-#im.write_text()
+# im.write_text()
