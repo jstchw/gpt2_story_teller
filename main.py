97b30d0db18549b6b930d73bd70c2ff0f7169cc2
jstchw <bobr703@gmail.com>
2022-05-03 12:49:49 +0100

    Final change

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
-# TODO:
-# - Conduct a survey - do people like it?
-# - Restructure the report
-# - Write an explanation of the testing in the apendices
-
-import nn
-import imgmanager
 from interface import Interface
-from nn import NN
-import time
-import numpy as np
+# from nn import NN
 
-network = NN('124M')
+# network = NN('124M')
 
-# network.train_model(file_name='datasets/alice/alice-preprocess.txt', run_name='alice-preprocess', steps=12000,
-# sample_length=100)
-
-# network.train_model(file_name='datasets/catcher/catcher-in-the-rye-new-format.txt', run_name='catcher', steps=8000,
+# network.train_model(file_name='datasets/the_iliad/the_iliad.txt', run_name='the_iliad', steps=12000,
                     # sample_length=100)
-
-# network.train_model(file_name='datasets/the_iliad/the_iliad.txt', run_name='the_iliad', steps=10000,
-# sample_length=100)
-
-# start_time = time.time()
-# text = nn.generate_text(run_name='catcher', length=100, nsamples=6)
-# print("--- %s seconds ---" % (time.time() - start_time))
 
 # Creates an instance of a browser using Eel.
 gui = Interface()
